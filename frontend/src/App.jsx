b90ce940b008b3eeca23ce8e0d6671f1cda068e7
mahfoozalamcse <amahfooz1007@gmail.com>
2025-11-17 13:43:45 +0530

    change file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/createPage";
 import NoteDetailPage from "./pages/noteDetailPage";
+
 
 
 const App = () => {
@@ -11,15 +11,12 @@ const App = () => {
       <div className="relative h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage/>}/>
-          <Route path="/note/:id" element={<NoteDetailPage/>}/>
+          <Route path="/" element = {<HomePage />} />
+          <Route path="/create" element = {<CreatePage/>}  />
+          <Route path="/note/:id" element = {<NoteDetailPage/>} />
         </Routes>
       </div>
-   
   );
 };
 
 export default App;
-
-
