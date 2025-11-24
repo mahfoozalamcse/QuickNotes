09f72dfa7e8e23404ef97040fe2fabf3c311c86d
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-11-24 15:58:10 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { PlusIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
+    
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
